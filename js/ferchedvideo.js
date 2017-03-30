952d6b543694d2c27872de5a9ef17d4f136e9379
Arunit Mazumdar <arunit95@gmail.com>
2017-03-30 11:38:44 +0530

    Update ferchedvideo.js

--- js/ferchedvideo.js
+++ js/ferchedvideo.js
@@ -2,39 +2,30 @@ var currentPage=0;
 var videoJson;
 
 function PageRender(idx){
-      var num=parseInt(idx);
-        $("video").attr("src",videoJson[num].src);
-        $("video").attr("poster",videoJson[num].cover);
-        $('header').text(videoJson[num].title);
-        console.log(videoJson[num].src);
-        console.log(videoJson[num].cover);
-        initializePlayer();
-      // timeBar.value=0;
-      // currentTime.innerHTML="0:00";
-      seekBar.value=0;
-      currentTime.innerHTML="0:00";
-      disableButton();
+    var num=parseInt(idx);
+    $("video").attr("src",videoJson[num].src);
+    $("video").attr("poster",videoJson[num].cover);
+    $('header').text(videoJson[num].title);       
+    initializePlayer();    
+    seekBar.value=0;
+    currentTime.innerHTML="0:00";
+    disableButton();
 };
-
 //getting the next page index
 function next(){
-        currentPage++;
-
-        disableButton();
-        //console.log(currentPage);
-        initializePlayer();
-        PageRender(currentPage);
-        stopPlay();
+     currentPage++;
+     disableButton();       
+     initializePlayer();
+     PageRender(currentPage);
+     stopPlay();
 };
 //getting the previous page index
 function prev(){
-        currentPage--;
-
-         disableButton();
-        // console.log(currentPage);
-        initializePlayer();
-          PageRender(currentPage);
-          stopPlay();
+      currentPage--;
+      disableButton();
+      initializePlayer();
+      PageRender(currentPage);
+      stopPlay();
 };
 //making next and prev button disabled
 function disableButton(){
@@ -51,28 +42,23 @@ function disableButton(){
     }
 };
 
-
-// var tocData = [];
 var getData = function(dataUrl) {
   return $.ajax({
     url: dataUrl,
     type:"GET",
     success:function(data,satus,xhr){
       return data;
-
      }
   });
 };
 
 $(document).ready(function() {
    var x = getData("js/fetchedVideo.json");
-   x.then(function(resp) {  //try
-     console.log(resp);
+   x.then(function(resp) {  //try    
      videoJson = resp;
      PageRender(0);
    })
-   .catch(function(err){  //catch
-    console.log(err);
+   .catch(function(err){  //catch   
    })
 
 });
